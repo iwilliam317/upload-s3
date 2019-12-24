778fe58a369b503a7b154564c87471bec913f5b2
Shinji Obana <willshinji@gmail.com>
2019-12-24 15:52:06 +0000

    configure multer upload*

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,5 +1,29 @@
 const multer = require('multer')
+const crypto = require('crypto')
+const path = require('path')
 
 module.exports = {
-    
+    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    storage: multer.diskStorage({
+
+    }),
+    limits: {
+        fileSize: 2 * 1024 + 1024
+    },
+    fileFilter: (req, file, cb) => {
+        const allowedMimeTypes = [
+            'image/jpeg',
+            'image/pjpeg',
+            'image.gif',
+            'image.png'
+        ]
+
+        if(allowedMimeTypes.includes(file.mimetype)){
+            cb(null, true)
+        }
+        else {
+            cb(new Error('Invalid file extension'))
+        }
+    }
+
 }
