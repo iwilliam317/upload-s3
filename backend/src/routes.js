6b11e67b1f1673ac146db409ea194c0661425092
Shinji Obana <willshinji@gmail.com>
2019-12-24 15:36:18 +0000

    configure multer upload*

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,8 @@
 const routes = require('express').Router()
+const multer = require('multer')
+const multerConfig = require('./config/multer')
 
-routes.get('/', (req, res) => res.json({"message": "Upload to S3"}))
+routes.get('/health', (req, res) => res.json({status: 200}))
+routes.post('/uploads', multer(multerConfig).single('file'), (req, res) => { res.send({body: req.body})})
 
 module.exports = routes
