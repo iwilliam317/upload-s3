95f7df97c448df1720cd51a9878f4990fb2ae524
Shinji Obana <willshinji@gmail.com>
2019-12-26 20:07:04 +0000

    set environment variables for S3

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
+require('dotenv').config()
+require('./config/db')
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'))
 app.use(require('./routes'))
 
-require('./config/db')
 
 app.listen(3000)
