9c2bbea275c5bf463605beb375e9dd15fd9b9d5f
Shinji Obana <willshinji@gmail.com>
2019-12-30 14:42:13 +0000

    route /DELETE

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,6 +12,16 @@ routes.get('/posts', async (req, res) => {
         res.status(500).send(error)
     }
 })
+
+routes.delete('/posts/:id', async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        await post.remove()
+        res.status(200).json({message: 'Post deleted!'})
+    } catch (error) {
+        
+    }
+})
 routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
     const {originalname: name, size, key} = req.file
     try {
