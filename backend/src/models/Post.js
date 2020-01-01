ab87f9c5fd6c49810b4be57da46543a6b118e5cf
Shinji Obana <willshinji@gmail.com>
2020-01-01 16:25:00 +0000

    remove files from local

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const aws = require('aws-sdk')
 const s3 = new aws.S3()
+const fs = require('fs')
+const path = require('path')
 
 const PostSchema = new mongoose.Schema({
     name: String,
@@ -21,8 +23,13 @@ PostSchema.pre('save', function () {
 
 PostSchema.pre('remove', function () {
     if(process.env.STORAGE_TYPE === 's3'){
-        var params = {Bucket: process.env.BUCKET_NAME, Key: this.key}
+        const params = {Bucket: process.env.BUCKET_NAME, Key: this.key}
         s3.deleteObject(params).promise()
+    } else{
+        fs.unlink(path.resolve(__dirname, '..', '..', 'tmp', 'uploads', this.key), function(err){
+            if (err) return err
+            console.log('removed from local')
+        })
     }
 })
 
