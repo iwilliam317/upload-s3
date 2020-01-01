12e37b8c3a58a641ed1e19514155eae1fa628f4f
Shinji Obana <willshinji@gmail.com>
2020-01-01 15:55:54 +0000

    minor refactoring

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/posts', (err, res) => {
+mongoose.connect(`${process.env.MONGO_URL}/posts`, (err, res) => {
     if (err) throw err
     console.log('MongoDB connected!')
 })
