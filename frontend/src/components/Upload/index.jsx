8b52921b05566872a16fc1043e578cd1b0882492
Shinji Obana <willshinji@gmail.com>
2020-01-11 20:19:40 +0000

    styling dragzone

--- frontend/src/components/Upload/index.jsx
+++ frontend/src/components/Upload/index.jsx
@@ -1,8 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import Dropzone from 'react-dropzone'
+import { DropContainer } from './styles'
 
 class Upload extends Component {
-    render(){
-        return <><h1>Hey</h1></>
+    render() {
+        return (<>
+            <h1>Hey</h1>
+            <Dropzone accept='image/*' onDropAccepted={acceptedFiles => console.log(acceptedFiles)}>
+                {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+                    <DropContainer
+                        {...getRootProps()}
+                        isDragActive={isDragActive}
+                        isDragReject={isDragReject}
+                    >
+                        <p>Drag 'n' drop some files here, or click to select files</p>
+                        <input {...getInputProps()} />
+                    </DropContainer>
+                )}
+            </Dropzone>
+        </>)
     }
 }
 
