fcb16c2e7b7e00cb5f9988d1a53c1a2ce488456f
Shinji Obana <willshinji@gmail.com>
2020-01-17 22:25:47 +0000

    sketching progressbar

--- frontend/src/components/FileList/index.jsx
+++ frontend/src/components/FileList/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, FileInfo, Preview } from './styles'
+import { CircularProgressbar } from 'react-circular-progressbar'
 
 const FileList = () => (
     <Container color='red'>
@@ -8,9 +9,16 @@ const FileList = () => (
                 <Preview src=''></Preview>
                 <div>
                     <strong>profile.png</strong>
-                    <span>64KB <button onClick={() => {}}>Delete</button></span>
+                    <span>64KB <button onClick={() => { }}>Delete</button></span>
                 </div>
             </FileInfo>
+            <div>
+                <CircularProgressbar
+                    strokeWidth={10} value={80} text={'80%'}
+                    styles={
+                        { root: { width: 24 }, path: { stroke: '#7159c1' } }
+                    } />
+            </div>
         </li>
     </Container>
 )
