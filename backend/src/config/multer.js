a1b66cbd5d617d7a5d2c63f75ad5ed5e0a62c607
Shinji Obana <willshinji@gmail.com>
2020-03-09 20:10:43 +0000

    increasing file size limit

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -37,7 +37,7 @@ module.exports = {
     dest: uploadDir,
     storage: storageTypes[process.env.STORAGE_TYPE],
     limits: {
-        fileSize: 2 * 1024 * 1024
+        fileSize: 10 * 1024 * 1024
     },
     fileFilter: (req, file, cb) => {
         const allowedMimeTypes = [
